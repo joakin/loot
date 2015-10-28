6fbd26eb99e6fbb8bea26428dbe8e09b45994db3
Sam Smith <git@samsmith.io>
2015-10-28 20:06:46 +0000

    Benchmark the LibXML version of the slim transform
    ... for comparison with the trumpet version.
    
    Replicate all remove* helper functions but removeDataMW and the slim
    top-level transform using libxmljs and a corresponding benchmark.

--- bench/bench.js
+++ bench/bench.js
@@ -1,16 +1,31 @@
 var Benchmark = require('Benchmark')
 var fs = require('fs')
-var slim = require('../lib/transforms/slim')
+var slimTrumpet = require('../lib/transforms/slim')
+var slimLibxml = require('../lib/transforms/libxml/slim')
+
+var suiteOptions = {
+  defer: true,
+  maxTime: 60
+}
 
 var suite = new Benchmark.Suite
 
-suite.add('slim', (deferred) => {
+suite.add('trumpet', (deferred) => {
   var html = fs.createReadStream(__dirname + '/Barack_Obama.html')
 
-  slim(html).on('end', () => {
+  slimTrumpet(html).on('end', () => {
     deferred.resolve()
   })
-}, { defer: true, maxTime: 60 })
+}, suiteOptions)
+
+suite.add('libxml', (deferred) => {
+  fs.readFile(__dirname + '/Barack_Obama.html', (err, html) => {
+    slimLibxml(html).toString()
+
+    deferred.resolve()
+  })
+}, suiteOptions)
+
 
 suite.on('cycle', (event) => console.log(String(event.target)))
 
