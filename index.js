07257c3fa8fe416dcb7dcaadce9c12ca68d155c0
Sam Smith <git@samsmith.io>
2015-12-16 13:41:07 +0000

    Rework the cache interface
    As part of the work for T120504 the cache will be varied one or more
    parameters and not simply the route and the title of the page.
    
    Changes:
    * Create an instance of the cache during initialization
    * Pass the instance of the cache as an argument to the route handler
      function so that they can read from and write to the cache
    * Create a cache key by hashing the route and title, which is somewhat
      simpler than before at the cost of an unintuitive cache directory

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 var http = require('http')
 var url = require('url')
-var mkdirp = require('mkdirp')
+
+var createCache = require('./lib/cache')
 
 var router = require('./lib/routes')
 
 var port = process.env.PORT || 7001
 
-mkdirp('./cache', function (err) {
-  // Ensure that the cache directory exists.
+createCache('./cache', function (err, cache) {
+  // Ensure that we have a cache
   if (err) throw err
+
   http.createServer((req, res) => {
     // Route request to corresponding endpoint
     var path = url.parse(req.url).pathname
     var match = router.match(path)
-    if (match) match.fn(req, res, match)
+    if (match) match.fn(req, res, match, cache)
     else { res.statusCode = 404; res.end('nope') }
   }).listen(port, () => console.log('Listening at', port))
 })
